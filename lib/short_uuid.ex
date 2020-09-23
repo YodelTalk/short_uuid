33a61b762a43a3f043791e825be90291247151e4
Mario Uher <uher.mario@gmail.com>
2020-09-23 16:30:43 +0200

    Init repo

---
+++ lib/short_uuid.ex
@@ -0,0 +1,18 @@
+defmodule ShortUuid do
+  @moduledoc """
+  Documentation for `ShortUuid`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ShortUuid.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/short_uuid_test.exs
@@ -0,0 +1,8 @@
+defmodule ShortUuidTest do
+  use ExUnit.Case
+  doctest ShortUuid
+
+  test "greets the world" do
+    assert ShortUuid.hello() == :world
+  end
+end
