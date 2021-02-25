073d473a5c8dae43397ab2eb0df2e17667dcd0de
Mario Uher <uher.mario@gmail.com>
2021-02-25 09:47:49 +0100

    Bump to 2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ShortUUID.MixProject do
   def project do
     [
       app: :short_uuid,
-      version: "1.0.0",
+      version: "2.0.0",
       elixir: ">= 1.5.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
